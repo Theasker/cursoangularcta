8dd3b0355cc4e6e4a2bafc136fa15acdafdb5ca4
Mauricio Segura <theasker@gmail.com>
2017-11-20 12:57:01 +0100

    ecommerce: starComponent

---
+++ ecommerce/src/app/shared/star.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+
+export class StarComponent implements OnInit, OnChanges {
+  @Input() rating: number;
+  @Output() ratingClicked: EventEmitter<any>;
+  starWidth: number;
+  nstar: number;
+
+  constructor() {
+    this.ratingClicked = new EventEmitter<any>();
+  }
+
+  ngOnInit() {  }
+
+  // Se ejecuta cuando cambia alguna de las propiedades de la clase
+  ngOnChanges(): void {
+    console.log('Cambio en las propiedades');
+    this.starWidth = this.rating * 86 / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit({
+      prop1: true,
+      name: 'texto',
+      code: 1234,
+      show: false
+    });
+  }
+}
